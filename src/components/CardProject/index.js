d5bd8476a41ef4b0ad5bc61a6b2056a40e885fbe
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-01 18:50:25 -0300

    feat(CardProject): style basic CardProject

--- src/components/CardProject/index.js
+++ src/components/CardProject/index.js
@@ -1,9 +1,30 @@
+import { Link } from 'react-router-dom'
+import { BiEdit } from 'react-icons/bi'
+import { FiDelete } from 'react-icons/fi'
+import style from './style.module.css'
 
 
 const CardProject = ({ id, name, budget, category, handleRemove  }) => {
 
     return(
-        <h1>{name}</h1>
+        <div className={style.project_card}>
+            <h4>{name}</h4>
+            <p>
+                <span>Orçamento: </span> R${budget}
+            </p>
+            <p className={style.category_text}>
+                <span className={`${style[category.name.toLowerCase()]}`}></span> {category.name}
+            </p>
+            <div className={style.project_card_actions}>
+                <Link to='/'>
+                    <BiEdit size={20} /> Editar
+                </Link>
+                <button>
+                    <FiDelete size={20} /> Excluir
+                </button>
+            </div>
+
+        </div>
     )
 }
 
